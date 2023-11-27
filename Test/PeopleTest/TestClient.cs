05d8d67819afc44fc47573d5715bde39509850e8
raphaelcordon <cordon@outlook.com>
2023-11-27 10:06:19 +0100

    Small overall tweaks

--- Test/PeopleTest/TestClient.cs
+++ Test/PeopleTest/TestClient.cs
@@ -1,18 +1,20 @@
+using Project.Controller.People;
 using Project.Entity.People;
+using Project.Interfaces.People;
 using Xunit;
 
 namespace Test.PeopleTest;
 
 public class TestClient
 {
+    IClient client = new ClientController();
     [Fact]
     public void TestCreateClientSuccessfully()
     {
-        var email = new EmailAddress("email@email.com");
-        var client = new Client("testName", "testSurname", email);
+        var client1 = client.CreateClient("testName", "testSurname", "email@email.com");
         
-        Assert.Equal("testName", client.Name);
-        Assert.Equal("testSurname", client.Surname);
-        Assert.Equal("email@email.com", client.Email.Email);
+        Assert.Equal("testName", client1.Name);
+        Assert.Equal("testSurname", client1.Surname);
+        Assert.Equal("email@email.com", client1.Email.Email);
     }
 }
--- Test/PeopleTest/TestEmployee.cs
+++ Test/PeopleTest/TestEmployee.cs
@@ -1,20 +1,22 @@
+using Project.Controller.People;
 using Project.Entity.People;
 using Project.Enum.People;
+using Project.Interfaces.People;
 using Xunit;
 
 namespace Test.PeopleTest;
 
 public class TestEmployee
 {
+    IEmployee employee = new EmployeeController();
     [Fact]
     public void TestCreateEmployeeSuccessfully()
     {
-        var email = new EmailAddress("email@email.com");
-        var employee = new Employee("testName", "testSurname", EmployeeRole.Admin, email);
+        var employee1 = employee.CreateEmployee("testName", "testSurname", EmployeeRole.Admin, "email@email.com");
         
-        Assert.Equal("testName", employee.Name);
-        Assert.Equal("testSurname", employee.Surname);
-        Assert.Equal(EmployeeRole.Admin, employee.EmployeeRole);
-        Assert.Equal("email@email.com", employee.Email.Email);
+        Assert.Equal("testName", employee1.Name);
+        Assert.Equal("testSurname", employee1.Surname);
+        Assert.Equal(EmployeeRole.Admin, employee1.EmployeeRole);
+        Assert.Equal("email@email.com", employee1.Email.Email);
     }
 }
